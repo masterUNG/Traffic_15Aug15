8ab096a612e478b74ec5d15e61ef647211625f11
masterUNG <phrombutr@gmail.com>
2015-08-15 15:25:29 +0700

    finish Day1

--- app/src/main/java/appewtc/masterung/traffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/traffic/MainActivity.java
@@ -1,16 +1,57 @@
 package appewtc.masterung.traffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial Widget
+        initialWidget();
+
+        //Button Controller
+        buttonController();
+
+    }   // Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_shut);
+                buttonPlayer.start();
+
+                //Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("http://androidthai.in.th/about-me.html"));
+                startActivity(objIntent);
+
+            }   // event
+        });
+    }
+
+    private void initialWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.btnAboutMe);
     }
 
     @Override
@@ -34,4 +75,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class  นี่คือ คลาสหลัก
